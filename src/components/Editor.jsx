8ab2c699e93a6521c7d274284198fb284e2e2b24
in kyung <kik87827@gmail.com>
2026-03-12 17:17:07 +0900

    init

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import "./Editor.css";
+
+const Editor = () => {
+  return (
+    <section className="Editor">
+      <input placeholder="새로운 Todo..." />
+      <button>추가</button>
+    </section>
+  );
+};
+
+export default Editor;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <h3>오늘은 📅</h3>
+      <h1>{new Date().toDateString()}</h1>
+    </header>
+  );
+};
+
+export default Header;
