36ded2fc76263bb48574d23521e1000d0ac30369
kik8727 <kik87827@gmail.com>
2026-06-26 23:55:28 +0900

    memo 스터디디

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
@@ -16,11 +16,30 @@ const List = ({ todos, onModify, onDelete }) => {
     return filterItem.length || search === "" ? filterItem : todos;
   };
 
+  
+
   const getFilterItems = filterItems();
+
+  // 의존성 배열 : deps
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData 호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
 
   return (
     <section className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>notDone : {notDoneCount}</div>
+      </div>
       <input
         placeholder="검색어를 입력하세요"
         value={search}
